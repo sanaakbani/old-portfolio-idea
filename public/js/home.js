ab8809ccade19bfd7cbac507278feaacfd7cfd4f
Sana Akbani <86171834+sanaakbani@users.noreply.github.com>
2021-09-16 11:15:12 -0500

    homepage changes

--- public/js/home.js
+++ public/js/home.js
@@ -11,36 +11,74 @@ $(document).ready(function() {
 
 $('.home').mouseover(function(){
     fadeIn(document.querySelector('.slider-home', 1));
+    document.querySelector('.slider-about').style.display = 'none';
+    document.querySelector('.slider-work').style.display = 'none';
+    document.querySelector('.slider-volunteer').style.display = 'none';
+    document.querySelector('.slider-projects').style.display = 'none';
+    document.querySelector('.slider-resume').style.display = 'none';
+    document.querySelector('.slider-contact').style.display = 'none';
+    
 });
 
 $('.about').mouseover(function(){
     fadeIn(document.querySelector('.slider-about', 1));
     document.querySelector('.slider-home').style.display = 'none';
+    document.querySelector('.slider-work').style.display = 'none';
+    document.querySelector('.slider-volunteer').style.display = 'none';
+    document.querySelector('.slider-projects').style.display = 'none';
+    document.querySelector('.slider-resume').style.display = 'none';
+    document.querySelector('.slider-contact').style.display = 'none';
 });
 
 $('.work').mouseover(function(){
     fadeIn(document.querySelector('.slider-work', 1));
     document.querySelector('.slider-home').style.display = 'none';
+    document.querySelector('.slider-about').style.display = 'none';
+    document.querySelector('.slider-volunteer').style.display = 'none';
+    document.querySelector('.slider-projects').style.display = 'none';
+    document.querySelector('.slider-resume').style.display = 'none';
+    document.querySelector('.slider-contact').style.display = 'none';
 });
 
 $('.volunteer').mouseover(function(){
     fadeIn(document.querySelector('.slider-volunteer', 1));
     document.querySelector('.slider-home').style.display = 'none';
+    document.querySelector('.slider-about').style.display = 'none';
+    document.querySelector('.slider-work').style.display = 'none';
+    document.querySelector('.slider-projects').style.display = 'none';
+    document.querySelector('.slider-resume').style.display = 'none';
+    document.querySelector('.slider-contact').style.display = 'none';
 });
 
 $('.projects').mouseover(function(){
     fadeIn(document.querySelector('.slider-projects', 1));
     document.querySelector('.slider-home').style.display = 'none';
+    document.querySelector('.slider-about').style.display = 'none';
+    document.querySelector('.slider-work').style.display = 'none';
+    document.querySelector('.slider-volunteer').style.display = 'none';
+    document.querySelector('.slider-resume').style.display = 'none';
+    document.querySelector('.slider-contact').style.display = 'none';
+    
 });
 
 $('.resume').mouseover(function(){
     fadeIn(document.querySelector('.slider-resume', 1));
     document.querySelector('.slider-home').style.display = 'none';
+    document.querySelector('.slider-about').style.display = 'none';
+    document.querySelector('.slider-work').style.display = 'none';
+    document.querySelector('.slider-volunteer').style.display = 'none';
+    document.querySelector('.slider-projects').style.display = 'none';
+    document.querySelector('.slider-contact').style.display = 'none';
 });
 
 $('.contact').mouseover(function(){
     fadeIn(document.querySelector('.slider-contact', 1));
     document.querySelector('.slider-home').style.display = 'none';
+    document.querySelector('.slider-about').style.display = 'none';
+    document.querySelector('.slider-work').style.display = 'none';
+    document.querySelector('.slider-volunteer').style.display = 'none';
+    document.querySelector('.slider-projects').style.display = 'none';
+    document.querySelector('.slider-resume').style.display = 'none';
 });
 
 $('.header-img').mouseover(function(){
